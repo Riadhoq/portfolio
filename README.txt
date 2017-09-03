1872c6c0b72d5af4a7ae68a71c5b694940e7cf07
Riadul Houqe <hoqueriadul@gmail.com>
2017-09-03 18:09:03 -0400

    New
    New Codes from CodePen

--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
-A Pen created at CodePen.io. You can find this one at http://codepen.io/Riadhoq/pen/RZKrmQ.
+A Pen created at CodePen.io. You can find this one at https://codepen.io/Riadhoq/pen/RZKrmQ.
 
  My portfolio page for freeCodeCamp.org  
--- js/index.js
+++ js/index.js
@@ -1,10 +1,35 @@
 $(document).ready(function() {
+    
+  window.onresize = function(e){
+    
+    if ($(window).width() < 640) {
+      $("#front-social i").removeClass('fa-3x');
+      $("#front-social i").addClass('fa-2x');
+    }  else  {  
+      $("#front-social i").removeClass('fa-2x');
+      $("#front-social i").addClass('fa-3x');  
+    }
+  }
+   
+  
+  window.onscroll = function (e) {  
+$("#to-top").removeClass("hidden"); 
+  if(document.body.scrollTop === 0){
+  $("#to-top").addClass("hidden");
+  }
+} 
   $("body").scrollspy({ target: ".navbar", offset: 50 });
 });
+
 
 $("#myNavbar a").on("click", function(event) {
   if (this.hash !== "") {
     event.preventDefault();
+
+    if (!$(this).hasClass("active")) {
+      $(".active").removeClass("active");
+      $(this).addClass("active"); 
+    }
 
     var hash = this.hash;
 
@@ -18,4 +43,9 @@ $("#myNavbar a").on("click", function(event) {
       }
     );
   }
+});
+
+$(".nav a").on("click", function() {
+  $(".nav").find(".active").removeClass("active");
+  $(this).parent().addClass("active");
 });
